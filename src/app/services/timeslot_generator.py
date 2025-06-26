f42f65f311e81e1b28f1edecfe831f562d38e80a
DmytroDD2 <vv676888@gmail.com>
2025-06-26 19:06:04 +0300

    update: modified slot logic1

--- src/app/services/timeslot_generator.py
+++ src/app/services/timeslot_generator.py
@@ -5,10 +5,32 @@ from fastapi import HTTPException
 from sqlalchemy import select
 from sqlalchemy.orm import Session
 
+from src.app.models.addon import Addon
 from src.app.models.appointment import Appointment
 from src.app.models.barber_schedule import BarberSchedule
 from src.app.models.barber_unavailable_time import BarberUnavailableTime
 from src.app.models.service import Service
+
+
+
+def calculate_total_duration(db: Session, service_id: int, addon_ids: list) -> timedelta:
+    # Получаем основную услугу
+
+    service = db.get(Service, service_id)
+    if not service:
+        raise ValueError("Service not found")
+
+    # Длительность основной услуги
+    service_duration = timedelta(minutes=service.duration)
+
+    # Суммируем длительность всех добавочных услуг
+    addons_duration = sum(
+        (addon.duration for addon in db.scalars(
+            select(Addon).where(Addon.id.in_(addon_ids))
+        ).all())
+    )
+    return service_duration + timedelta(minutes=addons_duration)
+
 
 
 def get_available_timeslots(
@@ -16,9 +38,10 @@ def get_available_timeslots(
         barber_id: int,
         target_date: date,
         service_id: int,
-        slot_interval_minutes: int = 15,
+        slot_interval_minutes: int = 50,
+        addon_ids: List[int] = None
 ) -> List[datetime]:
-    if slot_interval_minutes <= 0 or 60 % slot_interval_minutes != 0:
+    if slot_interval_minutes <= 0:
         raise ValueError("slot_interval_minutes must be a positive integer and a divisor of 60.")
 
     day_of_week = target_date.weekday()
@@ -27,7 +50,7 @@ def get_available_timeslots(
         .where(BarberSchedule.barber_id == barber_id)
         .where(BarberSchedule.day_of_week == day_of_week)
     ).first()
-
+    print(schedule_entry, "*" * 19,"barber_id", barber_id, "day_of_week", day_of_week)
     if not schedule_entry:
         return []
 
@@ -38,15 +61,20 @@ def get_available_timeslots(
     if not service:
         raise HTTPException(status_code=404, detail="Service not found")
 
-    service_duration_minutes = service.duration
-    service_duration = timedelta(minutes=service_duration_minutes)
+    # service_duration_minutes = service.duration
+    # service_duration = timedelta(minutes=service_duration_minutes)
 
+    total_duration = calculate_total_duration(db=db, service_id=service_id, addon_ids=addon_ids)
+    print("total_duration", total_duration)
+    # print("duration", total_duration, "service_duration_minutes", service_duration_minutes)
     working_start_datetime_utc = datetime.combine(target_date, working_start_time).replace(tzinfo=timezone.utc)
     working_end_datetime_utc = datetime.combine(target_date, working_end_time).replace(tzinfo=timezone.utc)
-
+    print("working_start_datetime_utc", working_start_datetime_utc)
+    print("working_end_datetime_utc", working_end_datetime_utc)
     start_of_day_utc = datetime.combine(target_date, datetime.min.time()).replace(tzinfo=timezone.utc)
     end_of_day_utc = datetime.combine(target_date, datetime.max.time()).replace(tzinfo=timezone.utc)
-
+    print("start_of_day_utc", start_of_day_utc)
+    print("end_of_day_utc", end_of_day_utc)
     existing_appointments = db.scalars(
         select(Appointment)
         .where(Appointment.barber_id == barber_id)
@@ -54,17 +82,20 @@ def get_available_timeslots(
         .where(Appointment.scheduled_time < end_of_day_utc + timedelta(microseconds=1))
     ).all()
 
+    print("existing_appointments", [appt.__dict__ for appt in existing_appointments])
+    print("-" * 30)
     booked_intervals = []
 
     for appt in existing_appointments:
         start_utc = appt.scheduled_time.astimezone(timezone.utc)\
             if appt.scheduled_time.tzinfo else appt.scheduled_time.replace(tzinfo=timezone.utc)
         end_utc = start_utc + timedelta(minutes=appt.total_duration)
+        print("+" * 30, end_utc, "+" * 30)
         booked_intervals.append({
-            "start": end_utc,
+            "start": start_utc,
             "end": end_utc
         })
-
+        print(booked_intervals[0])
     unavailable_intervals_db = db.scalars(
         select(BarberUnavailableTime)
         .where(BarberUnavailableTime.barber_id == barber_id)
@@ -74,7 +105,8 @@ def get_available_timeslots(
         .where(
             BarberUnavailableTime.start_time < datetime.combine(target_date, datetime.min.time()) + timedelta(days=1))
     ).all()
-
+    print("unavailable_intervals_db", [appts.__dict__ for appts in unavailable_intervals_db])
+    print("*" * 30)
     for unavail_time in unavailable_intervals_db:
         start_utc = unavail_time.start_time.astimezone(
             timezone.utc) if unavail_time.start_time.tzinfo else unavail_time.start_time.replace(tzinfo=timezone.utc)
@@ -87,7 +119,7 @@ def get_available_timeslots(
         })
 
     booked_intervals.sort(key=lambda x: x["start"])
-
+    print("booked_intervals", booked_intervals)
     merged_booked_intervals = []
     if booked_intervals:
         current_merged = booked_intervals[0]
@@ -99,25 +131,36 @@ def get_available_timeslots(
                 merged_booked_intervals.append(current_merged)
                 current_merged = next_interval
         merged_booked_intervals.append(current_merged)
-
+    print("new_booked_intervals", merged_booked_intervals)
     available_slots = []
     current_slot_start = working_start_datetime_utc
     start_minute = current_slot_start.minute
     if start_minute % slot_interval_minutes != 0:
         current_slot_start += timedelta(minutes=(slot_interval_minutes - (start_minute % slot_interval_minutes)))
         current_slot_start = current_slot_start.replace(second=0, microsecond=0)
-    while current_slot_start + service_duration <= working_end_datetime_utc:
-        potential_slot_end = current_slot_start + service_duration
+
+    while current_slot_start + total_duration <= working_end_datetime_utc:
+        # print(current_slot_start)
+        potential_slot_end = current_slot_start + total_duration
         is_available = True
 
         for booked in merged_booked_intervals:
+            # print("=====", booked["start"], potential_slot_end, booked["end"])
             if not (potential_slot_end <= booked["start"] or current_slot_start >= booked["end"]):
                 is_available = False
+                current_slot_start = booked["end"] + timedelta(minutes=slot_interval_minutes)
+                print("===" * 30, booked["start"], booked["end"])
                 break
 
         if is_available:
+            print("hello")
             available_slots.append(current_slot_start)
-
-        current_slot_start += timedelta(minutes=slot_interval_minutes)
+            # print(current_slot_start, "total_duration", total_duration, "potential_slot_end", potential_slot_end)
+            current_slot_start +=  total_duration + timedelta(minutes=slot_interval_minutes)
 
     return available_slots
+
+
+
+
+
